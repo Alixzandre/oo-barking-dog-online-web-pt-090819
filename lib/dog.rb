5be73d376f6f177c03e97f9b3dfafa3a72c18082
Alexander Bispo <alexbispo173@gmail.com>
2019-09-18 02:52:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,10 @@
 # Your code goes here!
 class Dog
-  def name
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
 end
